220d90979887776b2a55e7c5f550a5ce4d471f40
L4XB <lukas.buck@e-mail.de>
2025-01-23 18:50:33 +0000

    documentation: added file with used resscources

--- src/game.py
+++ src/game.py
@@ -1,12 +1,12 @@
-from .constants import PLAYER_ONE_SYMBOL, PLAYER_TWO_SYMBOL, AMOUNT_COLUMNS, AMOUNT_ROWS
-from src.env.game_board import GameBoard
+from constants import PLAYER_ONE_SYMBOL, PLAYER_TWO_SYMBOL, AMOUNT_COLUMNS, AMOUNT_ROWS
+from env.game_board import GameBoard
 
 board = GameBoard(AMOUNT_ROWS, AMOUNT_COLUMNS)
 
-board.insert_token(3, 'X')
-board.insert_token(3, 'O')
-board.insert_token(0, 'X')
-board.insert_token(6, 'O')
+board.insert_token(3, PLAYER_ONE_SYMBOL)
+board.insert_token(3, PLAYER_TWO_SYMBOL)
+board.insert_token(0, PLAYER_ONE_SYMBOL)
+board.insert_token(6, PLAYER_TWO_SYMBOL)
 
 
 board.draw_board()
