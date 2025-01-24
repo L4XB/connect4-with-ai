d2efadd7ad77930e5b4601b9554e6496c3fbd0d1
L4XB <lukas.buck@e-mail.de>
2025-01-24 10:48:56 +0000

    smart agent: update

--- src/agents/smart_agent.py
+++ src/agents/smart_agent.py
@@ -15,9 +15,41 @@ class SmartAgent:
         self.opponent_symbol = PLAYER_TWO_SYMBOL if symbol == PLAYER_ONE_SYMBOL else PLAYER_ONE_SYMBOL
 
     def get_move(self, board):
+        # TODO: Implement
         return False
 
-    def _check_winning_move(self, board, token):
+    def _is_winning_move(self, board, tkn):
+        """
+        the method [check_winner] checks if a player with a given token [tkn] has won, 
+        if so the method return True if not the method return False.
+        a player won if he has four of his tokens in a row/column or diagonal connected.
+        """
+        
+        # checks horizontal lines
+        for row in range(self.rows):
+            for col in range(self.cols - 3):
+                if all(board[row][col + i] == tkn for i in range(4)):
+                    return True
+
+        # checks vertical lines
+        for col in range(self.cols):
+            for row in range(self.rows - 3):
+                if all(board[row + i][col] == tkn for i in range(4)):
+                    return True
+
+        # check diagonals from the top right to the bottom left
+        for row in range(self.rows - 3):
+            for col in range(self.cols - 3):
+                if all(board[row + i][col + i] == tkn for i in range(4)):
+                    return True
+
+        # checks diagonals from the left top to the right bottom
+        for row in range(self.rows - 3):
+            for col in range(3, self.cols):
+                if all(board[row + i][col - i] == tkn for i in range(4)):
+                    return True
+
+        # no winning line found
         return False
 
     def _random_move(self, board):
@@ -33,7 +65,9 @@ class SmartAgent:
         return random_move
     
     def _is_valid_move(self, board,  col):
+        # TODO: Implement
         return False
 
     def _get_next_open_row(self, board, col):
+        # TODO: Implement
         return False
