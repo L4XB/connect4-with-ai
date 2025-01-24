792882db63eb1ecbcc796eec09ae332e0d36bfda
L4XB <lukas.buck@e-mail.de>
2025-01-24 19:53:02 +0000

    docmentation: little ifxes

--- src/evalutation.py
+++ src/evalutation.py
@@ -1,7 +1,7 @@
 from evaluation.random_vs_smart import simulate_games_random_vs_smart
 from evaluation.smart_vs_minimax import simulate_games_minimax_vs_smart
 
-##### simmulations #####
+##### evaluations #####
 
 # -> random agent vs. smart agent
 # simulate_games_random_vs_smart(500)
