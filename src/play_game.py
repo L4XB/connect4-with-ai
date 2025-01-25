ef2e67b6625d2904465d44d712226af3c8e33a23
L4XB <lukas.buck@e-mail.de>
2025-01-25 16:38:59 +0000

    documentation: added comments to the method, thats gives the oportunity to play a game

--- src/play_game.py
+++ src/play_game.py
@@ -4,38 +4,59 @@ from enviroment.game_board import GameBoard
 from agents.mini_max_agent import MiniMaxAgent
 
 def play_game_vs_agent():
+    '''
+    the method [play_game_vs_agent] gives the oportunity to play as a humand against
+    an Agent. The agents starts with a move, after this move, the humand and the agent pick
+    their moves after each other until one of wins or its ends in a draw.
+    '''
     
+    # create gameboard and agent
     board = GameBoard(AMOUNT_ROWS, AMOUNT_COLUMNS)
     agent = MiniMaxAgent(AMOUNT_ROWS, AMOUNT_ROWS, PLAYER_ONE_SYMBOL)
     
+    # game interrations 
     while True:
         
+        # get a move from the agent
         agent_move = agent.get_move(board.board)
+        
+        #insert the agent move into the board
         board.insert_token(agent_move, agent.symbol)
+        
+        # draw the board with the inserted move
         board.draw_board()
         
+        # check if the agent won with that move
         if(board.check_winner(agent.symbol)):
             print("Agent Won!")
             break
         
+        # check if the board is full and the game ends in a draw
         if(board.is_draw()):
             print("Its a draw!")
             break
         
         # player move
         while True:
+            
+            # get player input
             player_move = int(input("Enter your move (column, e.g. '0'): "))
+            
+            # insert the move, if its an invalid move the player will be asked to enter a other move
             if(not board.insert_token(player_move, agent.opponent_symbol)):
                 print("Invalid Move")
             else:
                 break
-    
+        
+        # draw board again after the player move is inserted
         board.draw_board()
         
+        # check if the player won with that move
         if(board.check_winner(agent.opponent_symbol)):
             print("Player Won!")
             break
         
+        # check if the board is full and the game ends in a draw
         if(board.is_draw()):
             print("Its a draw!")
             break  
