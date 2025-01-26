68e9b313f30bbcee57a114630673803089954eef
L4XB <lukas.buck@e-mail.de>
2025-01-26 17:28:45 +0000

    core: deleted everythik because model losed 100% of games against the minimax agent

--- src/evalutation.py
+++ src/evalutation.py
@@ -8,3 +8,5 @@ from evaluation.smart_vs_minimax import simulate_games_minimax_vs_smart
 
 # -> smart agent vs. minimax agent
 # simulate_games_minimax_vs_smart(500)
+
+# -> minimax agent vs. ml agent
